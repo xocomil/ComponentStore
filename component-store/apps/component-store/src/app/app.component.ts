e5c69fcf96319b6abca63fb55542d3120bc60dfc
xocomil <xocomil@gmail.com>
2021-03-21 11:39:58 -0600

    set up nx workspace and angular app

---
+++ component-store/apps/component-store/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'component-store-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'component-store';
+}
