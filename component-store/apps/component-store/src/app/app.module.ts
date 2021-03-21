99d374813f99ed900313919a601fc72a674701ca
xocomil <xocomil@gmail.com>
2021-03-21 14:20:05 -0600

    add modules for application

--- component-store/apps/component-store/src/app/app.module.ts
+++ component-store/apps/component-store/src/app/app.module.ts
@@ -1,12 +1,12 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
