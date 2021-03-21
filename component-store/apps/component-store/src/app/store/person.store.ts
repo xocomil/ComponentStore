26139f423fa015b2a91e321b920c09df8376c07c
xocomil <xocomil@gmail.com>
2021-03-21 14:35:50 -0600

    add `loadPeople()` to `PersonStore`

--- component-store/apps/component-store/src/app/store/person.store.ts
+++ component-store/apps/component-store/src/app/store/person.store.ts
@@ -1,6 +1,23 @@
 import { Injectable } from '@angular/core';
+import { ComponentStore } from '@ngrx/component-store';
+import { Person } from '../models/person';
+
+export interface PersonState {
+  people: Person[];
+}
+
+const defaultState: PersonState = {
+  people: [],
+};
 
 @Injectable()
-export class PersonStore {
-  constructor() {}
+export class PersonStore extends ComponentStore<PersonState> {
+  constructor() {
+    super(defaultState);
+  }
+
+  readonly loadPeople = this.updater((state, people: Person[] | null) => ({
+    ...state,
+    people: people || [],
+  }));
 }
