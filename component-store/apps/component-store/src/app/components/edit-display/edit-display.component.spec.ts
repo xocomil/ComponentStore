d12429b73ba52c91be63241136f084d99916b4be
xocomil <xocomil@gmail.com>
2021-06-18 12:49:40 -0600

    fix tests and add a simple test to convert

--- component-store/apps/component-store/src/app/components/edit-display/edit-display.component.spec.ts
+++ component-store/apps/component-store/src/app/components/edit-display/edit-display.component.spec.ts
@@ -1,16 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MockComponent } from 'ng-mocks';
+import { SavePersonComponentComponent } from '../save-person-component/save-person-component.component';
 import { EditDisplayComponent } from './edit-display.component';
 
-describe('EditDisplayComponent', () => {
+fdescribe('EditDisplayComponent', () => {
   let component: EditDisplayComponent;
   let fixture: ComponentFixture<EditDisplayComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EditDisplayComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        EditDisplayComponent,
+        MockComponent(SavePersonComponentComponent),
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +24,26 @@ describe('EditDisplayComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set the person', () => {
+    const testPerson = {
+      name: 'Test Person',
+      birth_year: '0',
+      eye_color: 'test color',
+      id: 0,
+      hair_color: 'test color',
+      gender: 'test gender',
+      mass: '100kg',
+      height: '200m',
+      skin_color: 'test color',
+    };
+
+    component.person = testPerson;
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'Editing Test Person (0)'
+    );
   });
 });
