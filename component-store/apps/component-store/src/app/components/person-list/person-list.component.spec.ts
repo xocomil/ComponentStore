5a6ab7a18710e0b614f8f06d8a99cad4db99643d
xocomil <xocomil@gmail.com>
2021-08-11 17:46:40 -0600

    convert to spectator and add an example test for `editorId$`

--- component-store/apps/component-store/src/app/components/person-list/person-list.component.spec.ts
+++ component-store/apps/component-store/src/app/components/person-list/person-list.component.spec.ts
@@ -1,25 +1,46 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  createComponentFactory,
+  mockProvider,
+  Spectator,
+} from '@ngneat/spectator';
+import { Subject } from 'rxjs';
+import { scan } from 'rxjs/operators';
+import { PersonStore } from '../../store/person.store';
 import { PersonListComponent } from './person-list.component';
 
 describe('PersonListComponent', () => {
-  let component: PersonListComponent;
-  let fixture: ComponentFixture<PersonListComponent>;
+  let spectator: Spectator<PersonListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PersonListComponent ]
-    })
-    .compileComponents();
+  const editorId$ = new Subject<number>();
+
+  const personStoreStub: Partial<PersonStore> = {
+    editorId$,
+  };
+
+  const createComponent = createComponentFactory({
+    component: PersonListComponent,
+    providers: [mockProvider(PersonStore, personStoreStub)],
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PersonListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    spectator = createComponent();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(spectator).toBeTruthy();
+  });
+
+  it('should emit the values from PersonStore.editorId', () => {
+    let emittedIds: (number | undefined)[];
+
+    spectator.component.editorId$
+      .pipe(scan((acc, cur) => [...acc, cur], []))
+      .subscribe((ids) => (emittedIds = ids));
+
+    editorId$.next(1);
+    editorId$.next(15);
+    editorId$.next(undefined);
+
+    expect(emittedIds).toEqual([1, 15, undefined]);
   });
 });
