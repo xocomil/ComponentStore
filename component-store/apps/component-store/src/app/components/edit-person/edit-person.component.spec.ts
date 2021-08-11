dfa6c1564e9d682317df254c1e756e4c3878803e
xocomil <xocomil@gmail.com>
2021-08-11 17:05:15 -0600

    convert to spectator and add tests

--- component-store/apps/component-store/src/app/components/edit-person/edit-person.component.spec.ts
+++ component-store/apps/component-store/src/app/components/edit-person/edit-person.component.spec.ts
@@ -1,25 +1,51 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  createComponentFactory,
+  mockProvider,
+  Spectator,
+} from '@ngneat/spectator';
+import { PersonStore } from '../../store/person.store';
+import { Person } from './../../models/person';
 import { EditPersonComponent } from './edit-person.component';
 
 describe('EditPersonComponent', () => {
-  let component: EditPersonComponent;
-  let fixture: ComponentFixture<EditPersonComponent>;
+  let spectator: Spectator<EditPersonComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ EditPersonComponent ]
-    })
-    .compileComponents();
+  const testPerson: Person = {
+    name: 'Test Person',
+    id: 1,
+    birth_year: '123 BBY',
+    eye_color: 'hazel',
+    gender: 'unknown',
+    hair_color: 'none',
+    height: 'really short',
+    mass: '2 earths',
+    skin_color: 'beige',
+  };
+
+  const personStoreStub: Partial<PersonStore> = {
+    setEditedPerson: jasmine.createSpy('setEditedPerson'),
+  };
+
+  const createComponent = createComponentFactory({
+    component: EditPersonComponent,
+    providers: [mockProvider(PersonStore, personStoreStub)],
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(EditPersonComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    spectator = createComponent({ props: { person: testPerson } });
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(spectator).toBeTruthy();
+  });
+
+  describe('personEdited()', () => {
+    it('should call PersonStore.saveEditedPerson with the person input', () => {
+      const personStoreMock = spectator.inject(PersonStore);
+
+      spectator.component.personEdited();
+
+      expect(personStoreMock.setEditedPerson).toHaveBeenCalledWith(testPerson);
+    });
   });
 });
