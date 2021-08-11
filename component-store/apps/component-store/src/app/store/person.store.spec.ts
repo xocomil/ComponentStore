812790c10f1ebb72ce36a19447fdf87eb0b5a5a1
xocomil <xocomil@gmail.com>
2021-08-11 14:49:36 -0600

    add a test for `editPerson()` effect

--- component-store/apps/component-store/src/app/store/person.store.spec.ts
+++ component-store/apps/component-store/src/app/store/person.store.spec.ts
@@ -4,6 +4,44 @@ import { Person } from '../models/person';
 import { StarWarsApiService } from '../services/star-wars-api.service';
 import { PersonStore } from './person.store';
 
+const createTestPeople = (): Person[] => {
+  return [
+    {
+      id: 1,
+      birth_year: '123 BBY',
+      eye_color: 'blue',
+      gender: 'unspecified',
+      hair_color: 'purple',
+      height: 'really tall',
+      mass: '1200 kg',
+      name: 'Test Subject 1',
+      skin_color: 'blue',
+    },
+    {
+      id: 2,
+      birth_year: '123 BBY',
+      eye_color: 'blue',
+      gender: 'unspecified',
+      hair_color: 'purple',
+      height: 'really tall',
+      mass: '1200 kg',
+      name: 'Test Subject 2',
+      skin_color: 'blue',
+    },
+    {
+      id: 3,
+      birth_year: '123 BBY',
+      eye_color: 'blue',
+      gender: 'unspecified',
+      hair_color: 'purple',
+      height: 'really tall',
+      mass: '1200 kg',
+      name: 'Test Subject 3',
+      skin_color: 'blue',
+    },
+  ];
+};
+
 fdescribe('PersonStoreService', () => {
   let spectator: SpectatorService<PersonStore>;
 
@@ -52,42 +90,34 @@ fdescribe('PersonStoreService', () => {
       expect(allEmits).toEqual([[], people, []]);
     });
   });
-});
 
-const createTestPeople = (): Person[] => {
-  return [
-    {
-      id: 1,
-      birth_year: '123 BBY',
-      eye_color: 'blue',
-      gender: 'unspecified',
-      hair_color: 'purple',
-      height: 'really tall',
-      mass: '1200 kg',
-      name: 'Test Subject 1',
-      skin_color: 'blue',
-    },
-    {
-      id: 2,
-      birth_year: '123 BBY',
-      eye_color: 'blue',
-      gender: 'unspecified',
-      hair_color: 'purple',
-      height: 'really tall',
-      mass: '1200 kg',
-      name: 'Test Subject 2',
-      skin_color: 'blue',
-    },
-    {
-      id: 3,
-      birth_year: '123 BBY',
-      eye_color: 'blue',
-      gender: 'unspecified',
-      hair_color: 'purple',
-      height: 'really tall',
-      mass: '1200 kg',
-      name: 'Test Subject 3',
-      skin_color: 'blue',
-    },
-  ];
-};
+  describe('editPerson()', () => {
+    const people = createTestPeople();
+
+    beforeEach(() => {
+      spectator.service.loadPeople(people);
+    });
+
+    it('should set the editorId and editedPerson in the state', () => {
+      let allIds: (number | undefined)[];
+      let allEditedPerson: Person[];
+
+      spectator.service.editorId$
+        .pipe(scan((acc: number[], cur) => [...acc, cur], []))
+        .subscribe((emits) => {
+          allIds = emits;
+        });
+
+      spectator.service.editedPerson$
+        .pipe(scan((acc: Person[], cur) => [...acc, cur], []))
+        .subscribe((emits) => {
+          allEditedPerson = emits;
+        });
+
+      spectator.service.editPerson(1); // We know ID 1 is in the array
+
+      expect(allIds).toEqual([undefined, 1]);
+      expect(allEditedPerson).toEqual([undefined, people[0]]);
+    });
+  });
+});
